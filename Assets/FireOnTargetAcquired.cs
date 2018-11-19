8bd92da8047318bb7e527ec347d7f31fb6c21373
RobinsonTyler <robinsontyler7@gmail.com>
2018-11-18 20:09:57 -0400

    enabled enemy AI to attack player

---
+++ Assets/FireOnTargetAcquired.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireOnTargetAcquired : MonoBehaviour {
+
+    public GameObject projectile;
+    public Transform player;
+
+    public float maxAngle;
+    public const float MISSILE_SPEED = 350;
+    public float cooldownTime;
+
+    private float cooldown;
+
+
+    private bool ShouldFire() {
+        float ang = Vector3.Angle(player.position - transform.position, transform.forward);
+        return Mathf.Abs(ang) < maxAngle;
+    }
+
+    private void FireMissile()
+    {
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.Play();
+        float dist = Vector3.Magnitude(player.position - transform.position);
+        Vector3 anticipatedPos = (player.position + (player.forward * dist / 5));
+        Vector3 v = (anticipatedPos - transform.position);
+        v.Normalize();
+        Quaternion q = Quaternion.LookRotation(v) * Quaternion.Euler(Vector3.right * 90);
+        var mis = Instantiate(projectile, transform.position, q);
+        mis.GetComponent<Rigidbody>().velocity = v * MISSILE_SPEED;
+        Destroy(mis, 10.0f);
+    }
+
+	void Start () {
+
+	}
+
+
+	void Update () {
+        float dist = Vector3.Magnitude(player.position - transform.position);
+        Vector3 anticipatedPos = (player.position + (player.forward * dist));
+		if (cooldown > 0)
+            cooldown -= Time.deltaTime;
+        else if (ShouldFire()) {
+            FireMissile();
+            cooldown = cooldownTime;
+        }
+	}
+}
